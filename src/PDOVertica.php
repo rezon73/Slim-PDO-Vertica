f8fec7cc99c124df0b59b8e49371679b6e62169c
karen <rezon73@gmail.com>
2021-07-07 16:12:19 +0300

    exec raw query method

--- src/PDOVertica.php
+++ src/PDOVertica.php
@@ -11,14 +11,24 @@ class PDOVertica
         $this->conn = odbc_connect($dsn, $user, $password);
     }
 
+    public function getConnection()
+    {
+        return $this->conn;
+    }
+
     public function prepare($qry)
     {
         return new PDOVerticaStatement($this->conn, $qry);
     }
 
+    public function exec($qry, $flags = null)
+    {
+        return odbc_exec($this->conn, $qry, $flags);
+    }
+
     public function lastInsertId()
     {
-        $stmt = odbc_prepare($this->conn, "SELECT LAST_INSERT_ID()");
+        $stmt = odbc_prepare($this->conn, 'SELECT LAST_INSERT_ID()');
         odbc_execute($stmt);
         $res = odbc_fetch_array($stmt);
         
